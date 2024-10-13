95078fb3e971a048a8f6696f2c8516ed11e938a2
Giovanni Karra <karragiovanni@gmail.com>
2024-10-13 16:22:05 +0200

    generalise repo so it can be used every year

---
+++ 2024/utils.py
@@ -0,0 +1,27 @@
+INF = float("inf")
+
+def printret(func):
+    def wrapper(*args):
+        val = func(*args)
+        print(val)
+        return val
+
+    return wrapper
+
+def printretarg(func):
+    def wrapper(*args):
+        val = func(*args)
+        print(f"args : {args}, ret : {val}")
+        return val
+
+    return wrapper
+
+def grid_to_string(grid):
+    return "\n".join(["".join(line) for line in grid])
+
+def product(l):
+    p = 1
+    for elem in l:
+        p *= elem
+
+    return p
