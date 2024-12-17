f573c27b30098c6bea9c1c398a0f71b8fbd4d200
Giovanni Karra <karragiovanni@gmail.com>
2024-12-17 11:49:05 +0100

    day 16 part 2

--- 2024/16.py
+++ 2024/16.py
@@ -1,5 +1,5 @@
 from utils import *
-from collections import defaultdict
+from collections import defaultdict, deque
 import heapq as hq
 
 input_str = open("input.txt", "r").read().strip("\n")
@@ -25,33 +25,63 @@ def get_start_and_dest(grid):
 def find_smallest_cost(grid, start, dest):
 	cost_move = 1; cost_rotate = 1000
 	costs = defaultdict(lambda: INF)
+	parents = defaultdict(list)
 	costs[start] = 0
 	heap = [(0, start)]
 	while heap:
 		cost, (position, rotation) = hq.heappop(heap)
 		if position == dest:
-			costs[position] = cost
-			break
+			costs[position] = min(cost, costs[position])
 		for direction in ((0, 1), (1, 0), (-1, 0), (0, -1)):
 			new_pos = (position[0]+direction[0], position[1]+direction[1])
 			if direction == rotation and inbound(grid, new_pos[0], new_pos[1]):
 				if (new_cost := cost + cost_move) < costs[(new_pos, direction)]:
 					costs[(new_pos, direction)] = new_cost
+					parents[(new_pos, direction)] = [(position, rotation)]
+					hq.heappush(heap, (new_cost, (new_pos, direction)))
+				elif new_cost == costs[(new_pos, direction)]:
+					costs[(new_pos, direction)] = new_cost
+					parents[(new_pos, direction)].append((position, rotation))
 					hq.heappush(heap, (new_cost, (new_pos, direction)))
 			elif direction != rotation:
 				if (new_cost := cost + cost_rotate) < costs[(position, direction)]:
 					costs[(position, direction)] = new_cost
+					parents[(position, direction)] = [(position, rotation)]
 					hq.heappush(heap, (new_cost, (position, direction)))
-	return costs[dest]
+				elif new_cost == costs[(new_pos, direction)]:
+					costs[(new_pos, direction)] = new_cost
+					parents[(new_pos, direction)].append((position, rotation))
+					hq.heappush(heap, (new_cost, (new_pos, direction)))
+	return costs, parents
 
 def part1():
 	grid, m, n = to_grid(input_str)
 	start, dest = get_start_and_dest(grid)
-	cost = find_smallest_cost(grid, start, dest)
-	print(cost)
+	costs, _ = find_smallest_cost(grid, start, dest)
+	print(costs[dest])
 
 def part2():
-	pass
+	grid, m, n = to_grid(input_str)
+	start, dest = get_start_and_dest(grid)
+	costs, parents = find_smallest_cost(grid, start, dest)
+	visited = set()
+	queue = deque()
+	for parent in [parents[(dest, (i, j))] for i, j in ((0, 1), (1, 0), (-1, 0), (0, -1)) if costs[(dest, (i, j))] == costs[dest]]:
+		for p in parent:
+			queue.append(p)
+	visited_intern = set()
+	while queue:
+		elem = queue.popleft()
+		if elem in visited_intern: continue
+		visited.add(elem[0])
+		visited_intern.add(elem)
+		print()
+		print(elem)
+		print(parents[elem])
+		print()
+		for parent in parents[elem]:
+			queue.append(parent)
+	print(len(visited)+1)
 
 part1()
 part2()
